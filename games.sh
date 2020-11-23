4688373a54765721273b6412701957491f7df3b7
Th3cr00k3dm4n <73489161+th3cr00k3dm4n@users.noreply.github.com>
2020-11-23 20:34:27 +0530

    Update games.sh

--- games.sh
+++ games.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 pkg=( "nsnake" "bastet" "nudoku" "moon-buggy" "pacman4console" )
-dpkg -s ${pkg[@]} 
-if [ $?=1 ]
- then
-pkg install ${pkg[@]} -y
-fi
-echo ""
-echo ""
-echo ""
-echo "Adding extra keys"
-echo ""
-echo ""
-echo ""
-mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1
-echo ""
-echo ""
-echo "loading................"
-echo ""
-echo ""
-echo ""
-echo ""
-clear
-echo ""
-echo  "█▀▀ ▄▀█ █▀▄▀█ █▀▀ █▀"
-echo  "█▄█ █▀█ █░▀░█ ██▄ ▄█"
-echo "                                      "
-echo  "                    th3cr00k3dm4n"
 
+banner()
+{
+
+echo ""
+echo "         █▀▀ ▄▀█ █▀▄▀█ █▀▀ █▀"
+echo "         █▄█ █▀█ █░▀░█ ██▄ ▄█"
+echo "                             "
+echo "                             °th3cr00k3dm4n"
+}
+
+run_script()
+{
+  banner
 
 PS3='SELECT A GAME : '
 options=("snake" "sudoku" "pacman" "moon-buggy" "tetris" "Quit")
@@ -72,3 +59,28 @@ break
     esac
 done
 
+}
+
+
+run_install()
+{
+    banner
+    read -p "Do you want to install missing libraries? [Y/n] :" ans
+    if [ $ans == "y" ]
+    then
+        {
+        mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1
+        pkg install ${pkg[@]} -y
+        if [ $? == 0 ]
+        then run_script
+        else  echo " failed try again" || run_install
+        fi
+        }
+     else echo "failed" || run_install
+    fi
+}
+
+dpkg -s ${pkg[@]} >/dev/null 2>&1 || run_install
+if [ $? == 0 ]
+then run_script
+fi
